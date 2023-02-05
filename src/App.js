4c7a8dde0f1b711e38a328a7b8fc58085713b55f
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-05 14:37:47 -0600

    converting icons to component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [wordToSearch, setWordToSearch] = useState("");
+
+  // later can save the old word the user has searched for before
+  // then prepopulate the data based on that
+
   return (
     <div className="brainbook">
-      <div className="container"></div>
+      <div className="container">
+        <nav className="nav"></nav>
+      </div>
     </div>
   );
 }
