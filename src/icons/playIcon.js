7c158202ba5d5ab1d60f407c6f6d3084d219b445
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-05 15:26:44 -0600

    working on icon structures

--- src/icons/playIcon.js
+++ src/icons/playIcon.js
@@ -6,7 +6,7 @@ const PlayIcon = () => {
       height="75"
       viewBox="0 0 75 75"
     >
-      <g fill="#A445ED" fill-rule="evenodd">
+      <g fill="#A445ED" fillRule="evenodd">
         <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
         <path d="M29 27v21l21-10.5z" />
       </g>
