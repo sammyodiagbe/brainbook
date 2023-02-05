0537680a47d82921555d2bc87a46614bc81a63de
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-05 15:58:01 -0600

    working on structure

--- src/icons/iconArrowDown.js
+++ src/icons/iconArrowDown.js
@@ -1,12 +1,14 @@
 const IconArrowDown = () => {
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="14"
-    height="8"
-    viewBox="0 0 14 8"
-  >
-    <path fill="none" stroke="#A445ED" strokeWidth="1.5" d="m1 1 6 6 6-6" />
-  </svg>;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="8"
+      viewBox="0 0 14 8"
+    >
+      <path fill="none" stroke="#A445ED" strokeWidth="1.5" d="m1 1 6 6 6-6" />
+    </svg>
+  );
 };
 
 export default IconArrowDown;
