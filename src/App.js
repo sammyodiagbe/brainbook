89fddd4666a8babeea9f43a5bb5e53c7bf2588da
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-02-06 10:28:32 -0600

    working on structure

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { useState } from "react";
 import IconArrowDown from "./icons/iconArrowDown";
 import Logo from "./icons/logo";
 import MoonIcon from "./icons/moonIcon";
+import NewWindowIcon from "./icons/newWindowIcon";
+import PlayIcon from "./icons/playIcon";
 import SearchIcon from "./icons/searchIcon";
 import { endpointUrl } from "./variables";
 
 function App() {
-  const [wordToSearch, setWordToSearch] = useState("");
+  const [wordToSearch, setWordToSearch] = useState("Keyboard");
   const [font, setFont] = useState("serif");
   const [darkTheme, useDarkTheme] = useState(false);
+  const [responseData, setResponseData] = useState({});
 
   // later can save the old word the user has searched for before
   // then prepopulate the data based on that
@@ -17,12 +20,60 @@ function App() {
     event.preventDefault();
     // do validation later
     if (!wordToSearch || wordToSearch === null) return;
-
-    const response = await fetch(endpointUrl(wordToSearch));
-    const data = await response.json();
-
-    console.log(data);
+    try {
+      const response = await fetch(endpointUrl(wordToSearch));
+      const data = await response.json();
+      const { resolution } = data;
+      if (resolution) {
+        // an error occured
+        console.log("An error has occured");
+        return;
+      }
+      setResponseData(data[0]);
+    } catch (error) {}
   };
+
+  const { word, meanings, phonetic, sourceUrls } = responseData;
+
+  const meaningStructure =
+    meanings && meanings.length
+      ? meanings.map((meaning, index) => {
+          const { partOfSpeech, definitions, antonyms, synonyms } = meaning;
+          return (
+            <div className="meaning" key={index}>
+              <h3 className="part-of-speech">{partOfSpeech}</h3>
+              <p className="meaning">Meaning</p>
+
+              <ul>
+                {definitions.map((definition, index) => {
+                  const { example, definition: def } = definition;
+
+                  return (
+                    <li>
+                      <span className="definition">{def}</span>
+                      {example && <span className="example">{example}</span>}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="syn-an">
+                {synonyms && synonyms.length ? (
+                  <div className="syn">
+                    <span>Synonyms </span>{" "}
+                    {synonyms.reduce((a, b) => a + ", " + b)}
+                  </div>
+                ) : null}
+                {antonyms && antonyms.length ? (
+                  <div className="syn">
+                    <span>Antonyms </span>{" "}
+                    {antonyms.reduce((a, b) => a + ", " + b)}
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          );
+        })
+      : null;
 
   return (
     <div
@@ -58,7 +109,32 @@ function App() {
             <SearchIcon />
           </div>
         </div>
-        <main className="content"></main>
+        <main className="content">
+          <div className="content-top">
+            <div className="left">
+              <h1>{word}</h1>
+              <p>{phonetic}</p>
+            </div>
+
+            <button className="audio">
+              <PlayIcon />
+            </button>
+          </div>
+          <div className="meaning-container">{meaningStructure}</div>
+        </main>
+        <div className="sources">
+          {sourceUrls &&
+            sourceUrls.map((source, index) => {
+              return (
+                <span key={index}>
+                  Source{" "}
+                  <a href={source} target="_blank">
+                    {source} {NewWindowIcon}
+                  </a>
+                </span>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
